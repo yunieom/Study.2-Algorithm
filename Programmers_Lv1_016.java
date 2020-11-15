a0a32e8db66e098a8d576169110f95136e4eac5a
yunieom <yunzoo0915@gmail.com>
2020-11-15 21:23:41 +0900

    Nov 15, 2020
    Programmers Lv.1

---
+++ Programmers_Lv1_016.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class Programmers_Lv1_016 {
+	public static void main(String[] args) {
+		
+		// 문자열을 정수로 바꾸기
+		
+		String s = "-1234";
+
+		int answer = Integer.parseInt(s);
+		System.out.println(answer);
+	
+        
+    }
+}

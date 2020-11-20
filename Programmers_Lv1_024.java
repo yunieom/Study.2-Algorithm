03ea206dde0e4ec260ce175d6ce764771e8a7d76
YOONJU EOM <yunzoo0915@gmail.com>
2020-11-20 14:36:23 +0900

    Update Programmers_Lv1_024.java

--- Programmers_Lv1_024.java
+++ Programmers_Lv1_024.java
@@ -10,14 +10,13 @@ public class Programmers_Lv1_024 {
 		int[] arr = {1,2,3,4};
 	
 		double answer = 0;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        
-        for(int i=0; i<arr.length; i++) {
-            answer += arr[i];
-        }
-		
-        answer = answer/arr.length;
-        //System.out.println(answer);
-		
+
+		for(int i=0; i<arr.length; i++) {
+		    answer += arr[i];
+		}
+
+		answer = answer/arr.length;
+		//System.out.println(answer);
+
 	}
 }

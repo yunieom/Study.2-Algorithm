868f7bf2bffaa43411a2e0a8ff70018ecc818ab0
yunieom <yunzoo0915@gmail.com>
2020-11-23 18:42:13 +0900

    Nov 23, 2020
    Programmers Lv.1

---
+++ Programmers_Lv1_027.java
@@ -0,0 +1,24 @@
+package programmers;import java.util.Arrays;
+
+public class Programmers_Lv1_027 {
+	public static void main(String[] args) {
+		
+		//  x만큼 간격이 있는 n개의 숫자
+		
+		long x = 2;
+		long n = 5; 
+		long temp[] = new long [(int)n+1]; // 반복문 시작이 1이므로 1칸 배열 1칸 추가
+		long answer[] = new long [(int)n];
+		
+	
+		for(int j=1; j<=n; j++) { // 1부터 배열의 인덱스는 n만큼이므로 n까지 돌기. 
+			temp[j] = x*j; // 받은 값에 1부터 n까지 곱해주기
+			for(int i=0; i<n; i++) {
+				answer[i] = temp[i+1]; // temp 배열은 인덱스 1부터 시작, i는 0부터 시작 -> i+1
+			}	
+		}
+		
+		System.out.println(Arrays.toString(answer));
+		
+	}
+}

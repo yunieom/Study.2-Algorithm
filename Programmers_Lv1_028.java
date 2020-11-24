19f72738f79f1065a2186764f00c501354d3d384
yunieom <yunzoo0915@gmail.com>
2020-11-25 07:31:46 +0900

    Nov 25, 2020
    Programmers Lv.1

---
+++ Programmers_Lv1_028.java
@@ -0,0 +1,44 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Programmers_Lv1_028 {
+	public static void main(String[] args) {
+		
+		// 제일 작은 수 제거하기
+
+		int arr [] = {10};
+	
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		int min = arr[0];
+		
+		for(int i=0; i<arr.length; i++) {
+			if(min > arr[i]) {
+				min = arr[i];
+			}
+			temp.add(arr[i]);
+		}
+		
+		for(int i=0; i<temp.size(); i++) {
+			if(temp.get(i) == min) {
+				temp.remove(i);
+			}
+			if(temp.isEmpty()) {
+				temp.add(0);
+			}
+		}
+		
+		int answer [] = new int [temp.size()];
+
+		for(int i=0; i<temp.size(); i++) {
+			answer[i] = temp.get(i);
+			if(temp.get(i)==0) {
+				answer[i]=-1;
+			}
+		}
+		System.out.println(Arrays.toString(answer));
+		
+		
+	}
+}
--- Programmers_Lv1_029.java
+++ Programmers_Lv1_029.java
@@ -1,45 +1,23 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Programmers_Lv1_029 {
 	public static void main(String[] args) {
 		
-		// 제일 작은 수 제거하기
+		// 자릿수 더하기
 
-		int arr [] = {10};
-	
-		ArrayList<Integer> temp = new ArrayList<Integer>();
-		int min = arr[0];
+		long n = 987;	
+		int answer = 0;
+		String s = n+"";
+		int arr [] = new int[s.length()];
+		
+		for(int i=0; i<s.length(); i++) {
+			arr[i] = s.charAt(i)-48;
+			System.out.println(arr[i]);
+		}
 		
 		for(int i=0; i<arr.length; i++) {
-			if(min > arr[i]) {
-				min = arr[i];
-			}
-			temp.add(arr[i]);
+			answer += arr[i];
 		}
-		
-		for(int i=0; i<temp.size(); i++) {
-			if(temp.get(i) == min) {
-				temp.remove(i);
-			}
-			if(temp.isEmpty()) {
-				temp.add(0);
-			}
-		}
-		
-		System.out.println(temp.isEmpty());
-		int answer [] = new int [temp.size()];
-
-		for(int i=0; i<temp.size(); i++) {
-			answer[i] = temp.get(i);
-			if(temp.get(i)==0) {
-				answer[i]=-1;
-			}
-		}
-		System.out.println(Arrays.toString(answer));
-		
-		
+		System.out.println(answer);
 	}
 }

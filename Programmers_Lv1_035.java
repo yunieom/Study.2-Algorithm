100c103babd4647276c205dd97321b30cf96b71f
yunieom <yunzoo0915@gmail.com>
2020-11-29 16:09:29 +0900

    Nov 29, 2020
    Programmers Lv.1

--- Programmers_Lv1_035.java
+++ Programmers_Lv1_035.java
@@ -3,18 +3,25 @@ package programmers;
 public class Programmers_Lv1_035 {
 	public static void main(String[] args) {
 		
-		//  내적 
+		//  하샤드 수 
 		
-		int a [] = {-1,0,1};
-		int b [] = {1,0,-1};
-		int answer = 0;
+		int x = 13;
+		boolean answer = true;
+		int a = 0;
 		
-		for(int i=0; i<a.length; i++) {
-			answer += a[i]*b[i];
+		String temp = x+"";
+		char arr [] = new char[temp.length()];
+		
+		for (int i=0; i<temp.length(); i++) {
+			arr[i]=temp.charAt(i);
+			a += arr[i]-48;
 		}
-		
+		if (x%a == 0) {
+			answer = true;
+		}else {
+			answer = false;
+		}
 		System.out.println(answer);
-
 		
 	}
 }
---
+++ Programmers_Lv1_036.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class Programmers_Lv1_036 {
+	public static void main(String[] args) {
+		
+		//  내적 
+		
+		int a [] = {-1,0,1};
+		int b [] = {1,0,-1};
+		int answer = 0;
+		
+		for(int i=0; i<a.length; i++) {
+			answer += a[i]*b[i];
+		}
+		
+		System.out.println(answer);
+
+		
+	}
+}

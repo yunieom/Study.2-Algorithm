9940b290328b0b2f2c985e67e1f03e5cb6ea205c
yunieom <yunzoo0915@gmail.com>
2023-05-05 00:38:24 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = 0
+    arr = s.split(' ')
+    for i in range(len(arr)):
+        if arr[i] != 'Z' :
+            answer += int(arr[i])
+        if arr[i] == 'Z' :
+            answer -= int(arr[i-1])
+        
+    return answer

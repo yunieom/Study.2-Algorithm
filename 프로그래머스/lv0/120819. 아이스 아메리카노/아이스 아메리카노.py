7d5433db66857798b3c83ec7854edc15790909c8
yunieom <yunzoo0915@gmail.com>
2023-05-08 16:51:42 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,6 +1,6 @@
-import math;
+
 def solution(money):
 
-    coffee = math.floor(money / 5500 )
-    change = money - (5500*coffee)
+    coffee = money // 5500
+    change = money % 5500
     return [coffee, change]

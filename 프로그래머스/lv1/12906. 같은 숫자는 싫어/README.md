c33de78791cf8153c64efbc96ae49eb48436a1e7
yunieom <yunzoo0915@gmail.com>
2023-05-15 22:32:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 117.11 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 27.9 MB, 시간: 120.48 ms
+메모리: 27.8 MB, 시간: 117.11 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,10 +1,8 @@
 def solution(arr):
     answer = []
     for i in range(0,len(arr)-1):
-        cur = arr[i]
-        nx = arr[i + 1]
-        if cur != nx:
-            answer.append(cur)
+        if arr[i] != arr[i+1]:
+            answer.append(arr[i])
     #print(arr[-1])
     answer.append(arr[-1])
     return answer

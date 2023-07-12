8c8629da4b1947dd25aa5ede566ea695ed6cf31c
yunieom <yunzoo0915@gmail.com>
2023-07-13 08:45:51 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -3,16 +3,14 @@ function solution(a, b, n) {
     let total = n;
     let cut = 0
 
-    while(total>a){
+    while(total>=a){
         cut = Math.floor(total/a);
         total = total-(a*cut)+(b*cut);
         answer += b*cut
     }
-    if(total===a){
-        answer+=b;
-    }
-
-
+    // if(total===a){
+    //     answer+=b;
+    // }
     return answer;
 }
 

ab3883b9c5dbc83819afff9c7b662dca0f2c65a5
yunieom <yunzoo0915@gmail.com>
2023-07-13 13:18:35 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,64 +1,16 @@
 function solution(lottos, win_nums) {
-    var answer = [];
-    let zero = lottos.filter(num => num === 0).length;
-    let min = 0;
-    
-    for (let i = 0; i < lottos.length; i++) {
-      if (lottos.includes(win_nums[i])) {
-        min++;
-      }
+  var answer = [];
+  let zero = lottos.filter(num => num === 0).length;
+  let min = 0;
+
+  for (let i = 0; i < lottos.length; i++) {
+    if (lottos.includes(win_nums[i])) {
+      min++;
     }
-    
-    let max = min + zero
-    switch (max) {
-      case 0:
-        answer[0] = 6;
-        break;
-      case 1:
-        answer[0] = 6;
-        break;
-      case 2:
-        answer[0] = 5;
-        break;
-      case 3:
-        answer[0] = 4;
-        break;
-      case 4:
-        answer[0] = 3;
-        break;
-      case 5:
-        answer[0] = 2;
-        break;
-      case 6:
-        answer[0] = 1;
-        break;
-    }
-    
-    switch (min) {
-      case 0:
-        answer[1] = 6;
-        break;
-      case 1:
-        answer[1] = 6;
-        break;
-      case 2:
-        answer[1] = 5;
-        break;
-      case 3:
-        answer[1] = 4;
-        break;
-      case 4:
-        answer[1] = 3;
-        break;
-      case 5:
-        answer[1] = 2;
-        break;
-      case 6:
-        answer[1] = 1;
-        break;
-    }
-   
-    
-    
-    return answer;
+  }
+
+  answer[0] = zero + min > 1 ? 7 - (zero + min) : 6;
+  answer[1] = min > 1 ? 7 - min : 6;
+
+  return answer;
 }

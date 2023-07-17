65de9879180364911b77822b8ccd6f92c7f5ea62
yunieom <yunzoo0915@gmail.com>
2023-07-17 21:22:50 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    const countOne = (n) => {
+        let binary = n.toString(2);
+        let count = 0;
+        for (let i = 0; i < binary.length; i++) {
+            if (binary[i] === '1') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    const targetCount = countOne(n); 
+    for (let i=n+1; ; i++) {
+        if (countOne(i) === targetCount) {
+            return i;
+        }
+    }
+}

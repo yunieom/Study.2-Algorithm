fe5f681c17e6af125453254e080319aedb12387a
yunieom <yunzoo0915@gmail.com>
2023-07-21 07:54:15 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,8 +1,10 @@
+
+
 function solution(s) {
     const arr = s.split(' ');
     let max = parseInt(arr[0])
     let min = parseInt(arr[0])
-    for(let i=0; i<arr.length; i++) {
+    for(let i=1; i<arr.length; i++) {
         max = max > parseInt(arr[i]) ? max : parseInt(arr[i]);
         min = min < parseInt(arr[i]) ? min : parseInt(arr[i]);
     }

1c7240b4010c2d1379271e7026b49f69b679e494
yunieom <yunzoo0915@gmail.com>
2023-08-25 05:38:34 +0900

    Time: 60 ms (49.43%), Space: 42.1 MB (73.30%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -4,7 +4,7 @@
  */
 var maxProfit = function(prices) {
     let profit = 0;
-    for(let i=1; i<prices.length; i++) { //i를 0에서 1로 바꾸는게 날듯
+    for(let i=1; i<prices.length; i++) { 
         if(prices[i]>prices[i-1]) {
             profit += prices[i]-prices[i-1];
         }
